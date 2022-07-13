1491df66a817ce98c2ef10b223e8129d23ac9d96
Angelo Rubin <angelorubin@gmail.com>
2022-07-13 16:59:50 -0300

    Refatorando index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
 import Providers from "./providers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Providers />
-  </React.StrictMode>,
-  document.getElementById("root")
+render(
+	<StrictMode>
+		<Providers />
+	</StrictMode>,
+	document.querySelector("#root")
 );
